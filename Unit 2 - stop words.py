205b157f74aa125fb6a5d30e2129dcf00f60ce63
Matus <matus123@gmail.com>
2016-01-10 20:06:12 +0100

    Starting unit 6

--- Unit 2 - stop words.py
+++ Unit 2 - stop words.py
@@ -6,4 +6,10 @@ from nltk.tokenize import word_tokenize
 example_sentence = "This is an example showing off stop word filtration."
 stop_words = set(stopwords.words("english"))
 
-print(stop_words)
+words = word_tokenize(example_sentence)
+filtered_sentence = []
+
+for w in words:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+print(filtered_sentence)
