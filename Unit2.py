505f123581554963df1d2976da221847fb17a2cd
Matus Karahuta <Matus Karahuta>
2016-01-10 18:38:53 +0100

    Initial commit

---
+++ Unit2.py
@@ -0,0 +1,9 @@
+__author__ = 'Matus'
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+example_sentence = "This is an example showing off stop word filtration."
+stop_words = set(stopwords.words("english"))
+
+print(stop_words)
